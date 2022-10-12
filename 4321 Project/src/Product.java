837f30edf0029033ac937d1b8f6df82d95e0fdab
Nakota Szymoniak <nakotaszymoniak@gmail.com>
2022-10-12 18:31:00 -0400

    Added quantity to products + its setter/getter, and changed some formating

--- 4321 Project/src/Product.java
+++ 4321 Project/src/Product.java
@@ -1,71 +1,73 @@
 
 public class Product {
 	
-	private String ProductName;
-	private double Price;
-	private String Manufacturer;
-	private int ProductID;
-	private boolean IsFood;
+	
+	private String productName;
+	private double price;
+	private String manufacturer;
+	private int productID;
+	private boolean isFood;
+	private int quantity;
 	
 	
 	//Constructor of the Product which has the name, price, manufacturer, id, and specifies if product is food.
-	public Product(String ProductName, double Price, String Manufacturer, int ProductID,  boolean IsFood) {
-		this.ProductName = ProductName;
-		this.Price = Price;
-		this.Manufacturer = Manufacturer;
-		this.ProductID = ProductID;
-		this.IsFood = IsFood;
-			
+	public Product(String productName, double price, String manufacturer, int productID,  boolean isFood, int quantity) {
+		this.productName = productName;
+		this.price = price;
+		this.manufacturer = manufacturer;
+		this.productID = productID;
+		this.isFood = isFood;
+		this.quantity = quantity;
 	}
+	
 	
 	//Getters
 	public String getProductName() {
-		return ProductName;
+		return productName;
 	}
 	public double getPrice() {
-		return Price;
+		return price;
 	}
 	public String getManufacturer() {
-		return Manufacturer;
+		return manufacturer;
 	}
 	public int getProductID() {
-		return ProductID;
+		return productID;
 	}
-
-	public boolean isIsFood() {
-		return IsFood;
+	public boolean getIsFood() {
+		return isFood;
 	}
-
+	public int getQuantity() {
+		return quantity;
+	}
 	
 	
 	//Setters
 	public void setProductName(String productName) {
-		ProductName = productName;
+		productName = productName;
 	}
 	public void setPrice(double price) {
-		Price = price;
+		price = price;
 	}
 	public void setManufacturer(String manufacturer) {
-		Manufacturer = manufacturer;
+		manufacturer = manufacturer;
 	}
 	public void setProductID(int productID) {
-		ProductID = productID;
+		productID = productID;
 	}
-	
 	public void setIsFood(boolean isFood) {
-		IsFood = isFood;
+		isFood = isFood;
+	}
+	public void setQuantity(int quantity) {
+		quantity = quantity;
 	}
 	
 
 	@Override
 	public String toString() {
-		
-		
-		
-		String ts = String.format("%s - %s , $ %d , ID - %d , SNAP Eligibility: %b \n", ProductName,
-				Manufacturer, Price, ProductID, IsFood);
+		String ts = String.format("%s - %s , $ %d , ID - %d , SNAP Eligibility: %b , %d\n", productName,
+				manufacturer, price, productID, isFood, quantity);
 		return ts;
 	}
-	
 
 }
