431faca7a6aed319b2d3f32750bea506730c0e09
Nakota Szymoniak <nakotaszymoniak@gmail.com>
2022-10-13 00:24:58 -0400

    No change

--- 4321 Project/src/Inventory.java
+++ 4321 Project/src/Inventory.java
@@ -70,10 +70,4 @@ public class Inventory {
 		inventory.get(productID).setQuantity(quantity);
 	}
 	
-	public boolean containsProduct(int productID){
-		return inventory.containsKey(productID);
-} 
-
-	
-
 }
