d99b7a8bbf03faf37531ed36b7cb1c29d9015ed2
Nakota Szymoniak <nakotaszymoniak@gmail.com>
2022-10-12 20:14:48 -0400

    built basic Customer class and added getters and setters

--- 4321 Project/src/Customer.java
+++ 4321 Project/src/Customer.java
@@ -1,4 +1,50 @@
 
 public class Customer {
 
+	private int customerID;
+	private String firstName;
+	private String lastName;
+	
+	public Customer(int customerID, String firstName, String lastName) {
+		this.customerID = customerID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	
+	//getters
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public int getfirstName() {
+		return firstName;
+	}
+	
+	public int getlastName() {
+		return lastName;
+	}
+	
+	
+	//setters
+	public void setCustomerID() {
+		customerID = customerID;
+	}
+	
+	public void setfirstName() {
+		firstName = firstName;
+	}
+	
+	public void setlastName() {
+		lastName = lastName;
+	}
+	
+	
+	@Override
+	public String toString() {
+		String ts = String.format("%d , %s , %s\n", customerID, firstName, lastName);
+		return ts;
+	}
+	
+	
 }
