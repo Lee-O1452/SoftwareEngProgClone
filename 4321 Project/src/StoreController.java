5779b9f4757763c051789423646e450695019c6d
Nakota Szymoniak <nakotaszymoniak@gmail.com>
2022-10-12 23:28:16 -0400

    created StoreController class and registerCustomer method

---
+++ 4321 Project/src/StoreController.java
@@ -0,0 +1,23 @@
+
+public class StoreController {
+
+	//private StoreGui gui;
+	private Store store;
+		
+//		public StoreController(StoreGui gui) {
+//			this.gui = gui;
+//			this.store = MapPersistence.buildStore();
+//		}
+		
+//		public void registerCustomer(int customerID, String firstName, string lastName) {
+//			boolean isValid = IDValidator.isIdValidCustomer(customerID);
+//			
+//			if(isValid) {
+//				if(!store.containsCustomer(customerID)) {
+//					Customer c = new Customer(customerID, firstName, lastName);
+//					store.addCustomer(c);
+//					//add confirmation here
+//				}
+//			}
+//		}
+}
