de153b048b230fe12f1a0d6ecdb22c1eac6becf2
Lee Owen <lee.owen1452@gmail.com>
2022-10-14 14:35:15 -0400

    Wrote productTest class.
    I have written the productTest class.
    Should be good to move to the next step of the test code process.
    
    Signed-off-by: Lee Owen <lee.owen1452@gmail.com>

--- 4321 Project/src/ProductTest.java
+++ 4321 Project/src/ProductTest.java
@@ -1,67 +1,94 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
+	Product x = new Product("Apple", 0.50, "Davie's Apple Orchard", 1001, true, 10);
 
 	@Test
-	void testProduct() {
-		Product x = new Product("Apple", 0.50, "Davie's Apple Orchard", 1001, true, 10);
-	}
-
-	@Test
+	@DisplayName("test getProductName()")
 	void testGetProductName() {
-		Product x = new Product("Apple", 0.50, "Davie's Apple Orchard", 1001, true, 10);
+		String expectedName = "Apple";
+		String actualName = x.getProductName();
+		assertEquals(expectedName, actualName);
 	}
 
 	@Test
+	@DisplayName("test getPrice()")
 	void testGetPrice() {
-		fail("Not yet implemented");
+		double expectedPrice = 0.50;
+		double actualPrice = x.getPrice();
+		assertEquals(expectedPrice, actualPrice);
 	}
 
 	@Test
+	@DisplayName("test getManufacturer()")
 	void testGetManufacturer() {
-		fail("Not yet implemented");
+		String expectedManufacturer = "Davie's Apple Orchard";
+		String actualManufacturer = x.getManufacturer();
+		assertEquals(expectedManufacturer, actualManufacturer);
 	}
 
 	@Test
+	@DisplayName("test getProductID()")
 	void testGetProductID() {
-		fail("Not yet implemented");
+		int expectedProductID = 1001;
+		int actualProductID = x.getProductID();
+		assertEquals(expectedProductID, actualProductID);
 	}
 
 	@Test
-	void testIsIsFood() {
-		fail("Not yet implemented");
+	@DisplayName("test getIsFood()")
+	void testIsFood() {
+		boolean expectedIsFood = true;
+		boolean actualIsFood = x.getIsFood();
+		assertEquals(expectedIsFood, actualIsFood);
 	}
 
 	@Test
+	@DisplayName("test setProductName()")
 	void testSetProductName() {
-		fail("Not yet implemented");
+		String expectedName = "Fish";
+		x.setProductName("Fish");
+		String actualName = x.getProductName();
+		assertEquals(expectedName, actualName);
 	}
 
 	@Test
+	@DisplayName("test setPrice()")
 	void testSetPrice() {
-		fail("Not yet implemented");
+		double expectedPrice = 1.00;
+		x.setPrice(1);
+		double actualPrice = x.getPrice();
+		assertEquals(expectedPrice, actualPrice);
 	}
 
 	@Test
+	@DisplayName("test setManufacturer()")
 	void testSetManufacturer() {
-		fail("Not yet implemented");
+		String expectedManufacturer = "Pete's Apple Farm";
+		x.setManufacturer("Pete's Apple Farm");
+		String actualManufacturer = x.getManufacturer();
+		assertEquals(expectedManufacturer, actualManufacturer);
 	}
 
 	@Test
+	@DisplayName("test setProductID()")
 	void testSetProductID() {
-		fail("Not yet implemented");
+		int expectedProductID = 0001;
+		x.setProductID(0001);
+		int actualProductID = x.getProductID();
+		assertEquals(expectedProductID, actualProductID);
 	}
 
 	@Test
+	@DisplayName("test setIsFood()")
 	void testSetIsFood() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testToString() {
-		fail("Not yet implemented");
+		boolean expectedIsFood = false;
+		x.setIsFood(false);
+		boolean actualIsFood = x.getIsFood();
+		assertEquals(expectedIsFood, actualIsFood);
 	}
 
 }
