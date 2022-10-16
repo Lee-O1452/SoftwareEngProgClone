47c9f9c9ccc364362c02ff495af3953ffa2a9c24
Nakota Szymoniak <nakotaszymoniak@gmail.com>
2022-10-16 18:44:07 -0400

    added testAddCustomer to StoreTest

--- 4321 Project/src/StoreTest.java
+++ 4321 Project/src/StoreTest.java
@@ -1,12 +1,36 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StoreTest {
 
+	Store store = new Store(01, "Greg's Store");
+	Customer c1 = new Customer(1257, "Doug", "Charles");
+	Customer c2 = new Customer(1258, "Charlene", "Holes");
+	Customer c3 = new Customer(1259, "Doug", "Holes");
+	Customer c4 = new Customer(1254, "Doug", "Holes");
+	Customer c5 = new Customer(1255, "Doug", "Holes");
+	Customer c6 = new Customer(1256, "Doug", "Holes");
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("test addCustomer()")
+	void testAddCustomer() {
+		store.addCustomer(c1);
+		boolean expectedCustomer = true;
+		boolean actualCustomer = store.containsCustomer(1255);
+		assertEquals(expectedCustomer, actualCustomer);
+	}
+	
+	@Test
+	@DisplayName("test displayCustomers()")
+	void testDisplayCustomers() {
+		store.addCustomer(c1);
+		store.addCustomer(c2);
+		store.addCustomer(c3);
+		store.addCustomer(c4);
+		store.addCustomer(c5);
+		store.addCustomer(c6);
 	}
 
 }
