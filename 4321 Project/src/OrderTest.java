9576bbe417af2caff3e88fb94a94e985966ae421
Munyeul Lee <mulee@valdosta.edu>
2022-10-21 22:53:02 -0400

    CustomerTest, OrderTest, IDValidatorTest, and change some code on IDValidator class

--- 4321 Project/src/OrderTest.java
+++ 4321 Project/src/OrderTest.java
@@ -2,11 +2,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class OrderTest {
+import java.util.Date;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+class OrderTest {
+    @SuppressWarnings("deprecation")
+    Order order
+            = new Order(12, 1234, new Date(2022, 10, 21),
+            true, false, new Store(11, "testStore"));
+
+    @Test
+    void test() {
+        assertEquals(12, order.getOrderID());
+        assertEquals(1234, order.getCustomerID());
+        assertTrue(order.getUsingSnap());
+        assertFalse(order.getisPaid());
+        assertEquals(11, order.getStore().getStoreID());
+        assertEquals("testStore", order.getStore().getStoreName());
+    }
 
 }
